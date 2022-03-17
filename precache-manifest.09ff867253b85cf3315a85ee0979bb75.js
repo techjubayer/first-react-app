47583bbf0f882a2e3852db3ba557a7527c3e7c6e
techjubayer <75694470+techjubayer@users.noreply.github.com>
2022-03-17 12:32:29 +0530

    Add files via upload

---
+++ precache-manifest.09ff867253b85cf3315a85ee0979bb75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c50d168ccb1b2024f6a8",
+    "url": "/techjubayer/first-react-app.git/static/css/main.48d653c2.chunk.css"
+  },
+  {
+    "revision": "c50d168ccb1b2024f6a8",
+    "url": "/techjubayer/first-react-app.git/static/js/main.c50d168c.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/techjubayer/first-react-app.git/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "b379251ef391182c8c6e",
+    "url": "/techjubayer/first-react-app.git/static/js/2.b379251e.chunk.js"
+  },
+  {
+    "revision": "a7b5c34506a0a6e2ef38",
+    "url": "/techjubayer/first-react-app.git/static/js/runtime~main.a7b5c345.js"
+  },
+  {
+    "revision": "1c79d283f882bea7a6b8ae6d319f0273",
+    "url": "/techjubayer/first-react-app.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/techjubayer/first-react-app.git/precache-manifest.09ff867253b85cf3315a85ee0979bb75.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/techjubayer/first-react-app.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
